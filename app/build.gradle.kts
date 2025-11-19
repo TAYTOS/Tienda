c9df10c9f219971c28a57755b35b47073a96c06f
Josh <jccahuanala@unsa.edu.pe>
2025-11-18 19:36:54 -0500

    update build configuration and remove unused preview function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.tienda"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.tienda"
